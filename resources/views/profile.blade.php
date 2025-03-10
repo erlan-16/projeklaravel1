34c81ae695d8519890537d81a08f1592369b40d1
erlan-16 <erlaneka005@gmail.com>
2025-03-10 12:10:23 +0700

    initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,29 @@
+<httml>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                <img src="Bung_amba.webp" alt="Profile Picture" width="150">
+
+            
+                <h4>Erlan Eka Putra Susanto</h4>
+                <p>Email: erlaneka005@gmail.com</p>
+                <p>Bio: Siswa SMKN 1 Sayung</p>
+                <h5>Skills:</h5>
+                        <ul>
+                                <li>Masak</li>
+                                <li>Kiper</li>
+                                <li>Pasang Lampu</li>
+                                <li>Pasang Gas</li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
